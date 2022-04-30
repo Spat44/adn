2c53d8749967f81fccd5b9a42c3635e17d018754
Julien-corazza <jcorazza@care-os.com>
2022-04-30 11:22:51 +0200

    Creating pages and init react router

---
+++ client/src/components/SubjectSuccess.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SubjectSuccess = () => {
+    return(
+        <div>
+            <h1>SubjectSuccess</h1>
+        </div>
+    );
+};
+
+export default SubjectSuccess;
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Welcome from './components/Welcome';
+import Subject from './components/Subject';
+import SubjectSuccess from './components/SubjectSuccess';
+import Candidates from './components/Candidates';
+
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/subject" element={<Subject />} />
+        <Route path="/subbject-success" element={<SubjectSuccess />} />
+        <Route path="/candidates" element={<Candidates />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
